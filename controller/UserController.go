023cdaba79569734c1515147562c0612f77ced60
lli <793437707@qq.com>
2022-01-29 01:49:43 +0800

    modified:   controller/UserController.go 	new file:   model/user.go 	modified:   vo/types.go

--- controller/UserController.go
+++ controller/UserController.go
@@ -2,8 +2,12 @@ package controller
 
 import (
 	"course_system/common"
+	"course_system/model"
+	"course_system/vo"
+	"errors"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
+	"net/http"
 )
 
 type IUserController interface {
@@ -40,5 +44,34 @@ func (ctl UserController) Update(c *gin.Context) {
 }
 
 func (ctl UserController) Delete(c *gin.Context) {
+	var req vo.DeleteMemberRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		panic(err.Error())
+	}
+
+	var user model.User
+	//检查用户不存在
+	if err := ctl.DB.First(&user, req.UserID).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.JSON(http.StatusOK, vo.DeleteMemberResponse{Code: vo.CourseNotExisted})
+			return
+		} else {
+			panic(err.Error())
+		}
+	}
+
+	//检查用户已删除
+	if(user.Enabled == 0){
+		c.JSON(http.StatusOK, vo.DeleteMemberResponse{Code: vo.UserHasDeleted})
+		return
+	}
+
+	//删除用户
+	if err := ctl.DB.Model(&user).Where("user_name = ?", req.UserID).Update("enabled","1").Error; err != nil {
+		panic(err.Error())
+	}
+
+	c.JSON(http.StatusOK,vo.DeleteMemberResponse{Code: vo.OK})
 
 }
