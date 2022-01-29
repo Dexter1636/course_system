7812ff0cf13434b1cbd9602dcfdc1f9fe2c6a872
Dexter <2779858398@qq.com>
2022-01-29 16:06:57 +0800

    feat: add BookCourse basically

--- controller/CourseBookingController.go
+++ controller/CourseBookingController.go
@@ -8,7 +8,9 @@ import (
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
+	"log"
 	"net/http"
+	"strconv"
 )
 
 type ICourseBookingController interface {
@@ -27,35 +29,60 @@ func NewCourseBookingController() ICourseBookingController {
 
 func (ctl CourseBookingController) BookCourse(c *gin.Context) {
 	var req vo.BookCourseRequest
+	code := vo.OK
+
+	// response
+	defer c.JSON(http.StatusOK, vo.BookCourseResponse{Code: code})
 
 	// validate data
 	if err := c.ShouldBindJSON(&req); err != nil {
-		panic(err.Error())
+		code = vo.ParamInvalid
+		return
+	}
+	studentId, err := strconv.ParseInt(req.StudentID, 10, 64)
+	if err != nil {
+		code = vo.ParamInvalid
+		return
+	}
+	courseId, err := strconv.ParseInt(req.CourseID, 10, 64)
+	if err != nil {
+		code = vo.ParamInvalid
+		return
 	}
 
 	// book course (v1: select for update)
 	// 1. check avail
-	// 2. delete avail
+	// 2. update avail
 	// 3. create sc record
-	err := ctl.DB.Transaction(func(tx *gorm.DB) error {
-		var course model.Course
-		if err := tx.Clauses(clause.Locking{Strength: "UPDATE"}).Select("avail").First(&course, req.CourseID); err != nil {
-			return errors.New("")
+	err = ctl.DB.Transaction(func(tx *gorm.DB) error {
+		// check avail
+		course := model.Course{Id: courseId}
+		if err := tx.Clauses(clause.Locking{Strength: "UPDATE"}).Select("avail").First(&course, courseId).Error; err != nil {
+			log.Println(err)
+			return err
 		}
-		//if course <= 0 {
-		//	return errors.New("")
-		//}
-		if err := tx.Model(&course).Updates("avail"); err != nil {
-			return errors.New("")
+		if course.Avail <= 0 {
+			code = vo.CourseNotAvailable
+			return errors.New("CourseNotAvailable")
+		}
+		// update avail
+		if err := tx.Model(&course).Update("avail", course.Avail).Error; err != nil {
+			log.Println(err)
+			return err
+		}
+		// create sc record
+		sc := model.Sc{
+			StudentId: studentId,
+			CourseId:  courseId,
+		}
+		if err := tx.Create(&sc).Error; err != nil {
+			return err
 		}
 		return nil
 	})
 	if err != nil {
 		return
 	}
-
-	// response
-	c.JSON(http.StatusOK, vo.BookCourseResponse{Code: vo.OK})
 }
 
 func (ctl CourseBookingController) GetStudentCourse(c *gin.Context) {
