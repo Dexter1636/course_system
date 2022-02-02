f3752819aebc2711e9fbf60e3d122798fdd28cf2
kuangshouren <1016153350@qq.com>
2022-02-03 00:16:01 +0800

    ;kldkl;saf;

--- router.go
+++ router.go
@@ -29,11 +29,11 @@ func RegisterRouter(r *gin.Engine) {
 	ccc := controller.NewCourseCommonController()
 	g.POST("/course/create", ccc.CreateCourse)
 	g.GET("/course/get", ccc.GetCourse)
-
-	g.POST("/teacher/bind_course")
-	g.POST("/teacher/unbind_course")
-	g.GET("/teacher/get_course")
-	g.POST("/course/schedule")
+	cbc := controller.NewCourseBookingController()
+	g.POST("/teacher/bind_course", cbc.Bind)
+	g.POST("/teacher/unbind_course", cbc.Unbind)
+	g.GET("/teacher/get_course", cbc.Get)
+	g.POST("/course/schedule", cbc.Schedule)
 
 	// 抢课
 	g.POST("/student/book_course")
