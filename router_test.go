05514294f3130a7e11c3298bf283806ceedc3f87
Dexter <2779858398@qq.com>
2022-02-03 16:00:41 +0800

    test: add TestPingRoute

---
+++ router_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var router *gin.Engine
+var pathPrefix string
+
+func setup() {
+	router = RegisterRouter()
+	pathPrefix = "/api/v1"
+}
+
+func teardown() {
+
+}
+
+func TestMain(m *testing.M) {
+	setup()
+	fmt.Println("Test begins....")
+	code := m.Run()
+	teardown()
+	os.Exit(code)
+}
+
+func TestPingRoute(t *testing.T) {
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", pathPrefix+"/ping", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "{\"message\":\"pong\"}", w.Body.String())
+}
