17dd5172fd0c1169b594a9785ffecc4e06f6515a
Dexter <2779858398@qq.com>
2022-02-17 16:12:52 +0800

    fix: params validator in CourseBooking

--- vo/validator.go
+++ vo/validator.go
@@ -11,6 +11,34 @@ func ValidateGetCourseReq(req GetCourseRequest) (code ErrNo) {
 	return ValidateStrCourseId(req.CourseID)
 }
 
+func ValidateBookCourseReq(req BookCourseRequest) (code ErrNo) {
+	code = ValidateStrStudentId(req.StudentID)
+	if code == OK {
+		code = ValidateStrCourseId(req.CourseID)
+	}
+	return code
+}
+
+func ValidateGetStudentCourseReq(req GetStudentCourseRequest) (code ErrNo) {
+	return ValidateStrStudentId(req.StudentID)
+}
+
+func ValidateStrStudentId(idStr string) (code ErrNo) {
+	stuId, err := strconv.ParseInt(idStr, 10, 64)
+	if err != nil {
+		return ParamInvalid
+	}
+	return ValidateStudentId(stuId)
+}
+
+func ValidateStudentId(id int64) (code ErrNo) {
+	if id > 0 {
+		return OK
+	} else {
+		return ParamInvalid
+	}
+}
+
 func ValidateStrCourseId(idStr string) (code ErrNo) {
 	courseId, err := strconv.ParseInt(idStr, 10, 64)
 	if err != nil {
